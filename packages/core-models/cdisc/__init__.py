f916d52dba1726c9fb744f735cf85d62f9efd931
Frederick de Ruiter <127706008+fderuiter@users.noreply.github.com>
2026-07-30 19:21:01 -0500

    feat(cdisc): build CDISC Library REST API client with API key authentication (#1023)

---
+++ packages/core-models/cdisc/__init__.py
@@ -0,0 +1,21 @@
+"""CDISC standards data models, library clients, and terminology cache."""
+
+from cdisc.cdisc_library_client import (
+    CdashDomainDefinition,
+    CdiscLibraryClient,
+    CdiscLibraryConfig,
+    CdiscProductSummary,
+    CodelistDefinition,
+    CodelistTerm,
+    SdtmDomainDefinition,
+)
+
+__all__ = [
+    "CdashDomainDefinition",
+    "CdiscLibraryClient",
+    "CdiscLibraryConfig",
+    "CdiscProductSummary",
+    "CodelistDefinition",
+    "CodelistTerm",
+    "SdtmDomainDefinition",
+]
